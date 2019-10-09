b68b2a4e5b9c379915ff5d192f6e9ecd5ab2310b
Dmitry Ivanov <dmitry.ivanov@devexperts.com>
2019-10-09 12:01:34 +0700

    test

---
+++ precache-manifest.970bd2ce7a3605e9a54d4ee4d3d93176.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3911137ff771125d02e",
+    "url": "/static/css/main.fac87596.chunk.css"
+  },
+  {
+    "revision": "c3911137ff771125d02e",
+    "url": "/static/js/main.c3db5d05.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79768f2c2aa20b2c3858",
+    "url": "/static/css/2.1524053a.chunk.css"
+  },
+  {
+    "revision": "79768f2c2aa20b2c3858",
+    "url": "/static/js/2.5d1fb99e.chunk.js"
+  },
+  {
+    "revision": "8593a5a07cf620d4512fcb71cbcd07a6",
+    "url": "/static/media/opensans-bold.8593a5a0.woff2"
+  },
+  {
+    "revision": "72862e7cf19603ad24f26baf86dd0e08",
+    "url": "/static/media/opensans-bold.72862e7c.woff"
+  },
+  {
+    "revision": "2c841cbc36bd6c715340513060c27f47",
+    "url": "/static/media/opensans-bolditalic.2c841cbc.woff"
+  },
+  {
+    "revision": "0a2de967793f1117d220464bb973a442",
+    "url": "/static/media/opensans-bolditalic.0a2de967.woff2"
+  },
+  {
+    "revision": "f588b35373729dd247d37254a8651d71",
+    "url": "/static/media/opensans-italic.f588b353.woff2"
+  },
+  {
+    "revision": "8a81b690262a2121feac9980efc331a4",
+    "url": "/static/media/opensans-italic.8a81b690.woff"
+  },
+  {
+    "revision": "48195e4b1b11a63230a49fc68b267b76",
+    "url": "/static/media/opensans-light.48195e4b.woff2"
+  },
+  {
+    "revision": "0d0d7107450f05b72a4507d0d7687dd1",
+    "url": "/static/media/opensans-light.0d0d7107.woff"
+  },
+  {
+    "revision": "ce659615885f33d928eb7fe276574106",
+    "url": "/static/media/opensans-regular.ce659615.woff"
+  },
+  {
+    "revision": "4124088fdd8c315a6d096b65b6cbf428",
+    "url": "/static/media/opensans-regular.4124088f.woff2"
+  },
+  {
+    "revision": "d8411d8bb1d6060de4fee4f3a20973cc",
+    "url": "/static/media/opensans-semibold.d8411d8b.woff2"
+  },
+  {
+    "revision": "49fbe77658d29a343cd06b5245be483e",
+    "url": "/static/media/opensans-semibold.49fbe776.woff"
+  },
+  {
+    "revision": "a5b0baef6011cd9663199e5f223244be",
+    "url": "/index.html"
+  }
+];
